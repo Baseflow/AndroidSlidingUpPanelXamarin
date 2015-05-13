d25cb6972e5c8977de32d8d625f0c99d2867fe54
Martijn van Dijk <mhvdijk@gmail.com>
2015-05-14 00:29:38 +0200

    First working version

---
+++ SlidingUpPanel.Sample/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Com.Sothree.Slidinguppanel;
+
+namespace SlidingUpPanel.Sample
+{
+    [Activity (Label = "SlidingUpPanel.Sample", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/AppBaseTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        private SlidingUpPanelLayout mLayout;
+
+        protected override void OnCreate (Bundle bundle)
+        {
+            base.OnCreate (bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            SetSupportActionBar(FindViewById<Toolbar>(Resource.Id.main_toolbar));
+
+
+            mLayout = FindViewById<SlidingUpPanelLayout>(Resource.Id.sliding_layout);
+        }
+    }
+}
+
+
